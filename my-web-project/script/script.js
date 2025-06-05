fbba99a85eff17e67369d254e5b7be3d44ec8915
juliasgr <julia_seger@estudante.sesisenai.org.br>
2025-06-05 19:12:02 -0300

    organização do código

--- my-web-project/script/script.js
+++ my-web-project/script/script.js
@@ -1,4 +1,4 @@
-const artigos = [
+/*const artigos = [
   {
     titulo: "A importância do TMO (tempo médio operacional) na satisfação do cliente",
     link: "artigos/tmo.html"
@@ -11,4 +11,12 @@ const artigos = [
     titulo: "Como otimizar serviços de TI em 7 passos",
     link: "artigos/otimizacao.html"
   }
-];
+];*/
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburguer");
+  const menu = document.querySelector("#menu");
+
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+});
